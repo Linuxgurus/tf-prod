89f29cbd722483c72d31897882a629f53d99170a
James Blackwell <jblack@linuxguru.net>
2021-11-29 00:41:10 -0800

    a lot built tonight

--- black/base_network/vpc.tf
+++ black/base_network/vpc.tf
@@ -5,3 +5,11 @@ module  "network" {
   app = ""
   name = var.cloud
 }
+
+
+module "subnet_ext1" {
+  source = "git@github.com:gbpn/terraform-common.git//subnet?ref=HEAD"
+  cloud = var.cloud
+  vpc_id = module.network.vpc_id
+}
+
